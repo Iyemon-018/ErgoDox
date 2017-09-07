357a55210c9c124df25e9cec4d5a45d75d1ce316
AYATAKA\Kishii <kishii@microcircus.com>
2017-09-07 14:29:15 +0900

    新規追加

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # ErgoDox
+
+http://rymo.hateblo.jp/entry/2017/05/02/170142
+
+
+## Bash on Ubuntu on Windows をインストールする。
+
+
+http://qiita.com/Aruneko/items/c79810b0b015bebf30bb
+
+## 各種ツールのインストール
+
+1. コマンドプロンプトを起動
+1. bash を入力
+1. 以下のコマンドを入力   
+
+sudo apt-get install git gcc-avr avr-libc dfu-programmer make  
+cd  
+git clone https://github.com/jackhumbert/qmk_firmware.git  
+
+
+## キーマップデータを編集する。
+
+Bash on Ubuntu on Windows のフォルダはいかの場所
+通常はフォルダが見えないが、エクスプローラーのパス部分に入力することで表示することが可能。
+
+C:\Users\<ユーザー名>\AppData\Local\lxss
+
+更に以下の改装まで潜るとデフォルトのキーマップがあるのでこれを編集する。
+自分はvim使ったこと無いので、VSCodeで以下のファイルをコピーして編集した。
+
+home\\<ユーザー名>\qmk_firmware\keyboards\ergodox_ez\keymaps\default
+
+## ビルド方法
+
+コマンドライン（Bash）で以下を入力する。
+
+cd ~/qmk_firmware/  
+make keyboard=ergodox_ez keymap=default  
+ls .build/ergodox_ez_default.hex
+
+すると以下のファイルが作成されるのでこれを使用してファームウェアを更新する。
+
+home\kishii\qmk_firmware\.build\ergodox_ezdefault.hex
+
+## 問題点
+
+１． Teensy.exe を２回実行するとUSBが認識しなくなる
+
+解決方法：
+PCを再起動する。
+
+２． カスタマイズしたファイルでビルドできないことがあった。
+
+解決策：
+1. qmk_firmware フォルダを削除する。
+1. bash を起動中であればフォルダを削除できないので終了しておく。
+1. リポジトリから最新版の環境を取得する。
+1. default/keymap.c を変更する。
+1. ビルドする。
+
+
+原因：
+不明
+今のところ、PCを再起動するとこの現象が発生する。
---
+++ keymap.c
@@ -0,0 +1,223 @@
+#include QMK_KEYBOARD_H
+#include "debug.h"
+#include "action_layer.h"
+#include "version.h"
+
+#define BASE 0 // default layer
+#define SYMB 1 // symbols
+#define MDIA 2 // media keys
+
+enum custom_keycodes {
+  PLACEHOLDER = SAFE_RANGE, // can always be here
+  EPRM,
+  VRSN,
+  RGB_SLD
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Keymap 0: Basic layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |  Esc   |   1  |   2  |   3  |   4  |   5  | PrtSC|           | `    |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  Esc |   Y  |   U  |   I  |   O  |   P  | BackSP |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  None  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ; + |  Enter |
+ * |--------+------+------+------+------+------| Hyper|           |  B   |------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | / ?  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | Ctrl | Win  | LAlt | Left | Right|                                       |  ^ ~ | : *  | [ {  |  ] } | \ _  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.        ,-------------.
+ *                                        |AltSh | Win  |       | Left | Right  |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Home |       |  Up  |        |      |
+ *                                 | Space|  Alt |------|       |------|  Tab   |Enter |
+ *                                 |      |      | End  |       | Down |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[BASE] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_PSCREEN,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_TRNS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LCTL,        KC_LGUI,      KC_LALT,KC_LEFT,KC_RGHT,
+                                               LALT(KC_LSFT), KC_LGUI,
+                                                              KC_HOME,
+                                               KC_SPC,KC_LALT,KC_END,
+        // right hand
+          KC_GRV,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+          KC_ESC,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSPC,
+                       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_ENT,
+          KC_B,        KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                               KC_EQL, KC_QUOT,KC_RBRC,KC_BSLS,          KC_RO,
+          KC_LEFT,        KC_RGHT,
+          KC_UP,
+          KC_DOWN,KC_TAB, KC_ENT
+    ),
+/* Keymap 1: Symbol Layer
+ *
+ * ,---------------------------------------------------.           ,--------------------------------------------------.
+ * |   Esc   |  F1  |  F2  |  F3  |  F4  |  F5  | PtrSC|           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
+ * |   Tab   |   !  |   @  |   {  |   }  |   |  |      |           | Esc  |      |      | Home |PageUp| @ / `|   F12  |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |         |   #  |   $  |   (  |   )  |   `  |------|           |------|      |  Del |  End |PageDw|   +  |  Enter |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  Shift  |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |      |  Up  |      |   \  | RShift |
+ * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | Ctrl  |  Win |  Alt |      |      |                                       | Left | Down | Right|   =  |      |
+ *   `-----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |AltShi| Win  |       | Left | Right|
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | Home |       |  Up  |      |      |
+ *                                 | Space| Alt  |------|       |------| Tab  | Enter|
+ *                                 |      |      | End  |       | Down |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// SYMBOLS
+[SYMB] = LAYOUT_ergodox(
+       // left hand
+       KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_PSCREEN,
+       KC_TAB, KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       KC_LSFT,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_LCTL,KC_LGUI,KC_LALT,KC_TRNS,KC_TRNS,
+                                       LALT(KC_LSFT), KC_LGUI,
+                                                      KC_HOME,
+                                       KC_SPC,KC_LALT,KC_END,
+       // right hand
+       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       KC_ESC,  KC_TRNS, KC_TRNS,KC_HOME, KC_PGUP, KC_LBRC, KC_F12,
+       KC_TRNS, KC_DEL, KC_END,  KC_PGDN, KC_PLUS, KC_ENT,
+       KC_TRNS, KC_AMPR, KC_TRNS,KC_UP,   KC_TRNS, KC_JYEN, KC_RSFT,
+                 KC_LEFT,KC_DOWN, KC_RGHT, KC_EQL,  KC_TRNS,
+       KC_LEFT,        KC_RGHT,
+       KC_UP,
+       KC_DOWN,KC_TAB, KC_ENT
+),
+/* Keymap 2: Media and mouse keys
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |Brwser|
+ *                                 |      |      |------|       |------|      |Back  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// MEDIA AND MOUSE
+[MDIA] = LAYOUT_ergodox(
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+                                           KC_TRNS, KC_TRNS,
+                                                    KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,
+    // right hand
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
+                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_WBAK
+),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        break;
+        case 1:
+        if (record->event.pressed) { // For resetting EEPROM
+          eeconfig_init();
+        }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    // dynamically generate these.
+    case EPRM:
+      if (record->event.pressed) {
+        eeconfig_init();
+      }
+      return false;
+      break;
+    case VRSN:
+      if (record->event.pressed) {
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+      }
+      return false;
+      break;
+    case RGB_SLD:
+      if (record->event.pressed) {
+        #ifdef RGBLIGHT_ENABLE
+          rgblight_mode(1);
+        #endif
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+
+};
+
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+
+    uint8_t layer = biton32(layer_state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+      // TODO: Make this relevant to the ErgoDox EZ.
+        case 1:
+            ergodox_right_led_1_on();
+            break;
+        case 2:
+            ergodox_right_led_2_on();
+            break;
+        default:
+            // none
+            break;
+    }
+
+};
